5bb0f98a2d7cb35ad5b29e594de1fa074f03d93b
AgustinSileoni <agustinsileoni98@gmail.com>
2022-01-31 11:11:32 -0300

    Caracteristica de crear usuarios funcionando

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# Importamos nuestros modelos
+from api import models
 
 # Register your models here.
+admin.site.register(models.Account)
--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
-import imp
-from django.shortcuts import render
 from httplib2 import Response
 #Imports necesarios
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+# Importamos nuestros forms y modelos
+from api import forms, models, serializers
+
 
 # Create your views here.
 # Devolvemos el mapa hello world en forma de JSON (el response utiliza JSON) en la parte del body
@@ -52,3 +54,19 @@ def test_bueno_malo(request):
         return Response( {"resuldato": "TRUE"},status= status.HTTP_200_OK)
     else :
         return Response( {"resuldato": "FALSE"},status= status.HTTP_400_BAD_REQUEST)
+
+# Definimos a la función con un POST
+@api_view(['POST'])
+def create_account(request):
+    # Para usar las forms le pasamos el objeto "request.POST" porque esperamos que sea
+    # un form que fue lanzado con un POST
+    form = forms.CreateUserForm(request.POST)
+    # Vemos si es válido, que acá verifica que el mail no exista ya
+    if form.is_valid():
+        # Guardamos el usuario que el form quiere crear, el .save() devuelve al usuario creado
+        user = form.save()
+        # Creamos la Account que va con el usuario, y le pasamos el usuario que acabamos de crear
+        models.Account.objects.create(user=user)
+        # Respondemos con los datos del serializer, le pasamos nuestro user y le decimos que es uno solo, y depués nos quedamos con la "data" del serializer
+        return Response(serializers.UserSerializer(user, many=False).data, status=status.HTTP_201_CREATED)
+    return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
